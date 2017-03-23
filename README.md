918a1f682e9d9b7bffc45bb736ba319955b22eb7
karla nava <karla.nm8@gmail.com>
2017-03-23 17:04:23 +0000

    Resultado final

---
+++ README.md
@@ -0,0 +1,16 @@
+# CALCULADORA
+
+## Comenzando por HTML
+
+### Cree un formulario con el nombre name
+### Dentro cree un div que contiene una tabla.
+### Dentro de la tabla ubiqué los botones de los números, coo de los operadores
+### Según se necesitaba, coloqué atributos como: id, value, type y los eventos.
+### Los eventos que utilicé son: onclick(en los botones) y onkeypress(sobre el input de texto o pantalla de la calculadora).
+
+
+## Ya en el JS
+### Cree una función mostrarNumero, que guarde los valores ingresados  o presionados, que se ven en la pantalla.
+### Hize una función signoIgual, que habilite al signo igual como el accionador de la operación.
+### Agregué una función validarNumero, que pudiera hacer que sólo los números y operadores, se pudieran usar en la calculadora.
+### Por último, cree la función eliminar, que elimina lo que hay en la pantalla.
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,22 +1,40 @@
 /*
+	Pseudocódigo:
 	1. Hacer que las teclas guarden su valor
+		-Guardar el valor de la pantalla
 	2. que la tecla = haga una función de hacer la operación
 	3. establecer qué operación hace cada signo
+		-encontré la funcion eval().
 	4. imprimir en el input el resultado de la operación
 	5. Que la calculadora acepte sólo los carácteres marcados.
+	6. El boton = y enter, tengan la misma función.
 */
 
 
 function mostrarNumero (num){
-    var guardarNumero = document.formulario.resultado.value;
-    document.formulario.resultado.value = guardarNumero + num;
-}
-
-function mostrarTecla () {
-	
-}
+    var guardarNumero = document.formulario.resultado.value; 
+    document.formulario.resultado.value = guardarNumero + num; 
+};
 
 function signoIgual (valor) {
 	var teclaPresionada = document.getElementById("textoPantalla");
 	teclaPresionada.value = eval(teclaPresionada.value);
+};
+
+function validarNumero (event) {
+	var ascii = event.keyCode	;
+
+	if (ascii == 13 ) {
+		signoIgual();
+		return false;
+	} 
+	if (ascii < 42 || ascii > 57 ) {
+		return false;
+	} ;
+}
+
+function eliminar(){ 
+ 	var anterior = document.formulario.contenedor.value;
+ 	var nuevoValor = anterior.substring(0,anterior.length-1);
+ 	document.getElementById("contenedor").value = nuevoValor;
 }
